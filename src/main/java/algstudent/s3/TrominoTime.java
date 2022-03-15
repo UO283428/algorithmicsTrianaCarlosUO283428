28284f83473b2052fbada56284ac802845fc9134
Carlos Triana Fernández <uo283428@uniovi.es>
2022-03-15 14:18:54 +0100

    Tromino Times code added

--- src/main/java/algstudent/s3/TrominoTime.java
+++ src/main/java/algstudent/s3/TrominoTime.java
@@ -2,7 +2,7 @@ package algstudent.s3;
 
 import java.util.Random;
 
-public class TrominoTimes {
+public class TrominoTime {
 
 	public static void main(String[] args) {
 		int reps = Integer.parseInt(args[0]);
